7a4590db2940ef5383459c8d1292bfec7af446bc
SinghalRish <singhalrishabh18@gmail.com>
2024-11-07 19:11:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Section2 from "./pages/Home/Section2";
+import Section3 from "./pages/Home/Section3";
+import Section5 from "./pages/Home/Section5";
+import Section6 from "./pages/Home/Section6";
+import Section7 from "./pages/Home/Section7";
+import Footer from "./components/Layouts/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Section2 />} />
+        <Route path="/menu" element={<Section3 />} />
+        <Route path="/shop" element={<Section5 />} />
+        <Route path="/blog" element={<Section6/>} />
+        <Route path="/contact" element={<Section7 />} />
+        
+      </Routes>
+    </Router>
+     
+      </>
   );
 }
 
